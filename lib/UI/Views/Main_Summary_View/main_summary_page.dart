cefb30f8ddad52d35b27c57a6668706b39c5b03d
Jacob Cockburn <jacobcockburn97@gmail.com>
2026-08-24 19:05:05 +0100

    Started work on dynamic date picker

--- lib/UI/Views/Main_Summary_View/main_summary_page.dart
+++ lib/UI/Views/Main_Summary_View/main_summary_page.dart
@@ -1,32 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 import 'package:weightly_app/Config/app_colours.dart';
 import 'package:weightly_app/UI/Core/Shared_Widgets/main_scaffold.dart';
+import 'package:weightly_app/UI/Views/Main_Summary_View/view_models/weightly_summary_view_model.dart';
+import 'package:weightly_app/UI/Views/Main_Summary_View/widgets/date_card.dart';
+import 'package:weightly_app/Utils/extensions.dart';
 
 class MainSummaryPage extends StatelessWidget {
-
   const MainSummaryPage({
     super.key
   });
 
   @override
   Widget build(BuildContext context) {
+    final summaryVM = context.watch<WeightlySummaryViewModel>();
+
     return WeightlyScaffold(
       appTitle: AppLocalizations.of(context)!.appTitle, 
       burgerNavBool: true,
       widgetBody: Column(
         children: <Widget> [
-          _dateCard(context),
+          Row(
+            children: <Widget> [
+              Expanded(
+                child: _dateCard(context, summaryVM.selectedDate , summaryVM.selectedDateRange, summaryVM)
+              )
+            ],
+          )
         ],
       )
       
     );
   }
 
-  Widget _dateCard(BuildContext context) {
+  Widget _dateCard(
+    BuildContext context,
+    DateTime selectedDate,
+    List<DateTime> dateList,
+    WeightlySummaryViewModel summaryVM
+    ) {
+    final int isoWeek = selectedDate.weekOfYear;
+    final String formatedMonth =  DateFormat.MMMM().format(selectedDate);
+
     return Container(
-      width: 500,
-      height: 150,
+      padding: EdgeInsets.fromLTRB(8, 12, 8, 12),
       decoration: BoxDecoration(
         color: context.getAppColours.surfaces.cardBackground,
         border: Border(
@@ -43,15 +62,47 @@ class MainSummaryPage extends StatelessWidget {
       child: Column(
         children: [
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-
+              IconButton(
+                onPressed: () => () , 
+                icon: Icon(
+                  Icons.keyboard_arrow_left,
+                  color: context.getAppColours.accent.primary,
+                  size: 32,
+                  )
+              ),
+              Column(
+                children: [
+                  Text(
+                    formatedMonth,
+                    style: TextStyle(
+                      color: context.getAppColours.text.primary,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold
+                    ),   
+                  ),
+                  Text(
+                    "${AppLocalizations.of(context)!.weekText} $isoWeek",
+                    style: TextStyle(
+                      color: context.getAppColours.text.secondary,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500
+                    ),
+                  )
+              ],
+              ),
+               IconButton(
+                onPressed: () => () , 
+                icon: Icon(
+                  Icons.keyboard_arrow_right,
+                  color: context.getAppColours.accent.primary,
+                  size: 32,
+                  )
+              ),
             ],
           ),
-          Row(
-            children: [
-
-            ],
-          )
+        DateCardWidget(selectedDate: selectedDate, dateTimeRange: dateList, viewModel: summaryVM,)
         ],
       ),
     );
---
+++ lib/UI/Views/Main_Summary_View/widgets/date_card.dart
@@ -0,0 +1,93 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:weightly_app/Config/app_colours.dart';
+import 'package:weightly_app/UI/Views/Main_Summary_View/view_models/weightly_summary_view_model.dart';
+import 'package:weightly_app/Utils/extensions.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class DateCardWidget extends StatefulWidget {
+  final WeightlySummaryViewModel viewModel;
+  final DateTime selectedDate;
+  final List<DateTime> dateTimeRange;
+
+  const DateCardWidget ({
+    super.key,
+    required this.selectedDate,
+    required this.dateTimeRange,
+    required this.viewModel,
+  });
+
+  @override
+  State<DateCardWidget> createState() => _DateCardWidget();
+}
+class _DateCardWidget extends State<DateCardWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 110,
+      child: Row(
+        children:
+            widget.dateTimeRange.map((date) {
+              return Expanded(
+                child: Container(
+                  width: 65,
+                  margin: const EdgeInsets.only(left: 5,right: 5, top: 5),
+                  decoration: BoxDecoration(
+                    color: widget.viewModel.isCurrentDate(date) ? 
+                      context.getAppColours.accent.primary : context.getAppColours.success.background,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 6),
+                  child: Column(
+                    children: [
+                      Text(
+                        Utils.truncateDayFormat(date.selectedDateFormat),
+                        style: TextStyle(
+                          color: widget.viewModel.isCurrentDate(date) ? 
+                            context.getAppColours.accent.currentWeightPillText : context.getAppColours.success.text,
+                          fontWeight: FontWeight.bold
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        date.dayFormat,
+                        style: TextStyle(
+                          color: widget.viewModel.isCurrentDate(date) ? 
+                            context.getAppColours.accent.currentWeightPillText : context.getAppColours.success.text,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        "Holder",
+                        style: TextStyle(
+                           color: widget.viewModel.isCurrentDate(date) ? 
+                            context.getAppColours.accent.currentWeightPillText : context.getAppColours.success.text,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 12
+                        ),
+                      ),
+                      if(widget.viewModel.isCurrentDate(date))
+                        Text(
+                          AppLocalizations.of(context)!.today,
+                          style: TextStyle(
+                           color: widget.viewModel.isCurrentDate(date) ? 
+                            context.getAppColours.accent.currentWeightPillText : context.getAppColours.success.text,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 12
+                        ),
+                        )
+                      
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
+      ),
+    );
+  }
+
+}
